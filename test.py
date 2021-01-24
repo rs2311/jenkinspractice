e139f22279a771af4ce67f3e0bd184f04aef0559
rs2311 <67817243+rs2311@users.noreply.github.com>
2021-01-24 11:29:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-file1 = open("pythoncreatedfile.yaml")
+#file1 = open("pythoncreatedfile.yaml")
+import sys
+with open('pythoncreated.yaml', 'a') as the_file:
+     the_file.write('%s\n'%sys.argv[1])
+    
+    
