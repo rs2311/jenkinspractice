389336ff8036f80157567a8c38c954dcc7af19da
rs2311 <67817243+rs2311@users.noreply.github.com>
2021-01-24 11:54:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 #file1 = open("pythoncreatedfile.yaml")
 import sys
 with open('pythoncreated.yaml', 'a') as the_file:
-     the_file.write('%s\n'%sys.argv[1])
-    
-    
+     the_file.write('"customers":  [ \n')
+     the_file.write('    { \n')
+     the_file.write('        "cust_id": "%s", \n'%sys.argv[2])
+     the_file.write('        "cust_name": "%s", \n'%sys.argv[1])
+     the_file.write('        "l3_vni": "%s", \n'%sys.argv[3])
+     the_file.write('        "l3_loopback_ip": "%s", \n'%sys.argv[4])
+     
