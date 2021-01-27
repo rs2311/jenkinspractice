b727dac1a811367b298931b7eea4b1a3f218e6c1
rs2311 <67817243+rs2311@users.noreply.github.com>
2021-01-27 09:26:56 +0530

    Update DCX_New_Cust_Intent_Update.py

--- DCX_New_Cust_Intent_Update.py
+++ DCX_New_Cust_Intent_Update.py
@@ -6,16 +6,16 @@ fname = "DCX_Intent.json"
 with open(fname) as f:
     newdct = json.load(f)
 
-print newdct['Devices']['CL31']['customers']
+print newdct['Devices'][SW]['customers']
 
-newdct['Devices']['CL31']['customers'].append({'cust_name': '%s'%sys.argv[1], 'cust_id': '%s'%sys.argv[2], })
+newdct['Devices'][SW]['customers'].append({'cust_name': '%s'%sys.argv[1], 'cust_id': '%s'%sys.argv[2], })
 
 print newdct['Devices']['CL31']['customers']
 
 with open(fname, "w") as f:
      json.dump(newdct, f)
 
-print newdct['Devices']['CL31']['customers']
+print newdct['Devices'][SW]['customers']
         
         
         
